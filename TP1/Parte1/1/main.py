0ab1f7abf9f9f122babfdf406c1fa0c9e0c6898f
Luciano Joaquin Albanes <lucianoalbanes@gmail.com>
2020-10-14 22:12:50 -0300

    Activity 1 - Done

---
+++ TP1/Parte1/1/main.py
@@ -0,0 +1,23 @@
+from algo1 import *
+
+#Ask lenght and declare vector (numbers)
+lenght = input_int('Ingrese la cantidad de elementos a analizar: ')
+numbers = Array(lenght, 0)
+
+#Ask element's values
+for i in range(0, lenght):
+    print('Posición actual (', i+1, '/', lenght, ')')
+    numbers[i] = input_int('Ingrese el valor: ')
+    
+#Find biggest absolute number
+biggestAbsNumPosition = int(0)
+for i in range(0, lenght):
+    actualAbsNum = abs(numbers[i])
+    biggestAbsNum = abs(numbers[biggestAbsNumPosition])
+    
+    if actualAbsNum > biggestAbsNum:
+        biggestAbsNumPosition = i
+    
+#Output phrase
+print('El elemento con mayor valor absoluto ingresado previamente es:', numbers[biggestAbsNumPosition])
+
