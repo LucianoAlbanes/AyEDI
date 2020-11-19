f1b34aa4287fc4b054395153a455184f6690c1e5
Luciano Joaquin Albanes <lucianoalbanes@gmail.com>
2020-11-19 15:21:23 -0300

    bubbleSort finished and test

---
+++ TP7/Pt1/bubbleSort.py
@@ -0,0 +1,35 @@
+from linkedlist import length, getNode, moveNode
+
+def bubbleSort(linkedList):
+    # Case empty list
+    if not linkedList.head:
+        return None
+    
+    # Define useful variables
+    lengthOfLList = length(linkedList)
+    lap = 0
+    flagChanges = True # to make a do-while
+    
+    # Sort
+    while flagChanges:
+        flagChanges = False
+        position = 0
+        actualNode = linkedList.head
+        # Check from head to last node - lap
+        for _ in range(0, lengthOfLList-lap):
+            # Check if next node of node has minor value.
+            if actualNode.value > actualNode.nextNode.value:
+                # Save the real actual node
+                actualNode = actualNode.nextNode
+                
+                # Move the nodes and update the flag
+                moveNode(linkedList, position+1, position)
+                flagChanges = True
+                
+            # Check if exist two nodes ahead. else break the current lap
+            if not (actualNode.nextNode and actualNode.nextNode.nextNode):
+                break
+            
+            actualNode = actualNode.nextNode
+            position += 1
+        lap += 1
---
+++ TP7/Pt1/main.py
@@ -0,0 +1,41 @@
+# ~~ Test ~~
+
+from linkedlist import add, access, LinkedList, length
+from bubbleSort import bubbleSort
+from selectionSort import selectionSort
+from insertionSort import insertionSort
+import random  # Only for testing
+
+def printList(linkedList):
+    print('[', end="")
+    for i in range(0, length(linkedList)):
+        if i != 0:
+            print(', ', end="")
+        print(str(access(linkedList, i)), end="")
+    print(']')
+
+def testSort(sorterFn):
+    # Print current sort method
+    print(f'\n~~~ {sorterFn.__name__} ~~~')
+    
+    # Create and fill the list
+    print('Generando una lista con 10 valores aleatorios...')
+
+    listOfNumbers = LinkedList()
+    for _ in range(0, 10):
+        add(listOfNumbers, random.randint(0, 10))
+
+    # Print unsorted list
+    printList(listOfNumbers)
+
+    # Sort
+    print('\nOrdenando la lista...')
+    sorterFn(listOfNumbers)
+
+    # Print sorted list
+    printList(listOfNumbers)
+
+## TESTS
+testSort(bubbleSort)
+testSort(selectionSort)
+testSort(insertionSort)
