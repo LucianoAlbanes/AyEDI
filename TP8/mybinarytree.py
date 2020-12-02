47df683888a403502e6c44643700d3e9b72f2fc9
Luciano Joaquin Albanes <lucianoalbanes@gmail.com>
2020-12-01 22:00:45 -0300

    TP8 finished

--- TP8/mybinarytree.py
+++ TP8/mybinarytree.py
@@ -1,16 +1,20 @@
-## Binary tree ADT implementation.
+# Binary tree ADT implementation.
+
+from linkedlist import LinkedList, add
+
 # Define classes.
 
 class BinaryTree:
-    root=None
+    root = None
 
 class BinaryTreeNode:
-    key=None
-    value=None
-    leftnode=None
-    rightnode=None
-    parent=None
+    key = None
+    value = None
+    leftnode = None
+    rightnode = None
+    parent = None
 
+# Define functions
 
 def search(binaryTree, element):
     '''
@@ -23,29 +27,37 @@ def search(binaryTree, element):
         The key of the node with the given element.
         If the element is found multiple times, returns the first key where appears. (Preorder)
         Returns 'None' if there is no a node with the given element in the binary tree.
-    '''    
+    '''
     # Search recursively using aux fn, store pointer and return the key.
-    foundNode = searchAux(binaryTree.root, element)
+    foundNode = searchAux(binaryTree.root, element, None)
     if foundNode:
         return foundNode.key
 
-def searchAux(actualNode, element):
+
+def searchAux(actualNode, element, leafCheck):
     # Empty node case.
     if not actualNode:
         return None
-    
+
     # Match case.
     if actualNode.value == element:
-        return actualNode
-    
+        if leafCheck:  # To use in the delete fn
+            if actualNode.leftnode or actualNode.rightnode:
+                return None
+            else:
+                return actualNode
+        else:
+            return actualNode
+
     # Recursive part.
     # Search in both sides and store the return.
-    foundNode = searchAux(actualNode.leftnode, element)
+    foundNode = searchAux(actualNode.leftnode, element, leafCheck)
     if not foundNode:
-        foundNode = searchAux(actualNode.rightnode, element)
-    
+        foundNode = searchAux(actualNode.rightnode, element, leafCheck)
+
     # Return the pointer of the node
     return foundNode
+
 
 def insert(binaryTree, element, key):
     '''
@@ -62,37 +74,39 @@ def insert(binaryTree, element, key):
     # Check if the key already exists.
     if checkKeyExistence(binaryTree, key):
         return None
-    
+
     # Create the new node
     newNode = BinaryTreeNode()
     newNode.key = key
     newNode.value = element
-    
+
     # Case if empty tree.
     if not binaryTree.root:
         binaryTree.root = newNode
         return newNode.key
-    
+
     # General case
     insertAux(binaryTree.root, newNode)
     return key
 
+
 def insertAux(actualNode, newNode):
-    #Left node case.
+    # Left node case.
     if actualNode.key > newNode.key:
-       if not actualNode.leftnode:
-           actualNode.leftnode = newNode
-           newNode.parent = actualNode
-       else:
-           insertAux(actualNode.leftnode, newNode)
-    
-    #Right node case.
+        if not actualNode.leftnode:
+            actualNode.leftnode = newNode
+            newNode.parent = actualNode
+        else:
+            insertAux(actualNode.leftnode, newNode)
+
+    # Right node case.
     if actualNode.key < newNode.key:
-       if not actualNode.rightnode:
-           actualNode.rightnode = newNode
-           newNode.parent = actualNode
-       else:
-           insertAux(actualNode.rightnode, newNode)
+        if not actualNode.rightnode:
+            actualNode.rightnode = newNode
+            newNode.parent = actualNode
+        else:
+            insertAux(actualNode.rightnode, newNode)
+
 
 def delete(binaryTree, element):
     '''
@@ -100,6 +114,7 @@ def delete(binaryTree, element):
         Delete an node with a given element on an binary tree.
     Info:
         If exist more than one node with the element, only the first one will be deleted. (Preorder)
+        Only can delete leaf nodes.
     Params:
         binaryTree: The binary tree on which you want to perform the delete.
         element: The element of the node of the binary tree to be deleted.
@@ -107,11 +122,25 @@ def delete(binaryTree, element):
         The key of the deleted node.
         Returns 'None' if there is no a node with the given element in the binary tree.
     '''
-    # Search the element
-    nodeToDelete = searchAux(binaryTree.root, element)
+    # Search the element, with check leaf
+    nodeToDelete = searchAux(binaryTree.root, element, True)
 
-def deleteAux(actualNode, element):
-    return
+    # Unsuccessful case
+    if not nodeToDelete:
+        return None
+
+    # Store the key of the node to delete
+    key = nodeToDelete.key
+
+    # Remove the node from the tree
+    if nodeToDelete is nodeToDelete.parent.leftnode:
+        nodeToDelete.parent.leftnode = None
+    else:
+        nodeToDelete.parent.rightnode = None
+
+    # Return the key
+    return key
+
 
 def checkKeyExistence(binaryTree, key):
     '''
@@ -126,22 +155,49 @@ def checkKeyExistence(binaryTree, key):
     # Case if empty tree.
     if not binaryTree.root:
         return False
-    
+
     # Search recursively using aux fn, and return a boolean.
     return checkKeyExistenceAux(binaryTree.root, key)
+
 
 def checkKeyExistenceAux(actualNode, key):
     # Empty node case.
     if not actualNode:
         return False
-    
+
     # Match case.
     if actualNode.key == key:
         return True
-    
+
     # Recursive part.
     # Search in the side where could exist a same key.
     if key < actualNode.key:
         return checkKeyExistenceAux(actualNode.leftnode, key)
     else:
         return checkKeyExistenceAux(actualNode.rightnode, key)
+
+
+def traverseInPreOrder(binaryTree):
+    # Empty tree case
+    if not binaryTree.root:
+        return None
+
+    # Create the linked list to storage the elements.
+    elements = LinkedList()
+
+    # Use an aux recursive fn to navigate the tree and store the elements.
+    traverseInPreOrderAux(binaryTree.root, elements)
+
+    # Return the filled linked list.
+    return elements
+
+
+def traverseInPreOrderAux(actualNode, linkedList):
+    # Recursively go to all nodes, both sides, right to left
+    if actualNode.rightnode:
+        traverseInPreOrderAux(actualNode.rightnode, linkedList)
+    if actualNode.leftnode:
+        traverseInPreOrderAux(actualNode.leftnode, linkedList)
+        
+    # Store element in the linked list
+    add(linkedList, actualNode.value)
